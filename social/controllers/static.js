1a78531d9ac21a003886ce2a52c775edebe59063
sjbgit <sjb_strat@hotmail.com>
2015-01-16 12:33:31 -0600

    posts working

--- social/controllers/static.js
+++ social/controllers/static.js
@@ -1,10 +1,13 @@
 /**
  * Created by sbunke on 1/15/2015.
  */
-var router = require('express').Router();
+var express = require('express')
+var router = express.Router()
 
 router.get('/', function (req, res) {
     res.sendfile('layouts/posts.html')
 })
+
+router.use(express.static(__dirname + '/../assets'));
 
 module.exports = router
